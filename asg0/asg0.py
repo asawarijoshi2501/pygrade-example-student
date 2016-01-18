fdbed0f9c1e2af16d2c4f159bfbbc9092644e721
Aron Culotta <aronwc@gmail.com>
2016-01-18 10:23:37 -0600

    add (incorrect) example solution

--- asg0/asg0.py
+++ asg0/asg0.py
@@ -4,8 +4,8 @@ A simple example of a student's assignment.
 
 def is_mammal(text):
     """ Return true if text is mammal. """
-    pass
+    return text == 'cat' or text == 'dog'
 
 
 def add(x, y):
-    pass
+    return x - y
